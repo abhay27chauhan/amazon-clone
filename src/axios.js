9dcd39623e7c4b6d527b0625624ee9ab31480116
Abhay <chauhan27.abhay@gmail.com>
2020-11-22 14:56:25 +0530

    updated server

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ac-amazon-clone.herokuapp.com/' // the API (cloud function) url
+    baseURL: 'http://localhost:5001/clone-d2f64/us-central1/api' // the API (cloud function) url
 });
 
 export default instance;
