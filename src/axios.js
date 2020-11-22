4ac333b19032440601cc4b1c4eeb24a9be7dc91e
Abhay <chauhan27.abhay@gmail.com>
2020-11-22 14:03:22 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-d2f64/us-central1/api' // the API (cloud function) url
+    baseURL: `${process.env.PORT}/clone-d2f64/us-central1/api` // the API (cloud function) url
 });
 
 export default instance;
